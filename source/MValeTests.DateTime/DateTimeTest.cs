5b598b104dabe6c0875008fae22ecfbfc7242807
Valentino <valentino@asistar.it>
2023-12-20 12:30:51 +0100

    Update date time test.

--- source/MValeTests.DateTime/DateTimeTest.cs
+++ source/MValeTests.DateTime/DateTimeTest.cs
@@ -4,53 +4,66 @@ using System.Linq;
 
 namespace MValeTests.DateTime;
 
-public class Tests
+/// <summary>
+///     Constructs a DateTimeOffset from a DateTime. For Local and Unspecified kinds,
+///     extracts the local offset. For UTC, creates a UTC instance with a zero offset.
+///     public DateTimeOffset(DateTime dateTime) {
+///         TimeSpan offset;
+///         if (dateTime.Kind != DateTimeKind.Utc) {
+///             // Local and Unspecified are both treated as Local
+///             offset = TimeZoneInfo.GetLocalUtcOffset(dateTime, TimeZoneInfoOptions.NoThrowOnInvalidTime);
+///         }
+///         else {
+///             offset = new TimeSpan(0);
+///         }
+///         m_offsetMinutes = ValidateOffset(offset);
+///         m_dateTime = ValidateDate(dateTime, offset);
+///     }
+/// </summary>
+public class DateTimeTest
 {
-    public class DateTimeTest
+    [Test]
+    public static void Test()
     {
-        [Test]
-        public static void Test()
+        var dto = DateTimeOffset.UtcNow;
+        var dtoPlus2 = dto.ToOffset(TimeSpan.FromHours(2));
+        var udt = dto.UtcDateTime;
+        var ldt = dto.LocalDateTime;
+        var dt = dto.DateTime;
+        var udtToDto = new DateTimeOffset(udt);
+        var ldtToDto = new DateTimeOffset(ldt);
+        var dtToDto = new DateTimeOffset(dt);
+
+        var values = new Dictionary<string, object>()
         {
-            var dto = DateTimeOffset.UtcNow;
-            var dtoPlus2 = dto.ToOffset(TimeSpan.FromHours(2));
-            var udt = dto.UtcDateTime;
-            var ldt = dto.LocalDateTime;
-            var dt = dto.DateTime;
-            var udtToDto = new DateTimeOffset(udt);
-            var ldtToDto = new DateTimeOffset(ldt);
-            var dtToDto = new DateTimeOffset(dt);
+            [nameof(dto)] = dto,
+            [nameof(dtoPlus2)] = dtoPlus2,
+            [nameof(udt)] = udt,
+            [nameof(ldt)] = ldt,
+            [nameof(dt)] = dt,
+            [nameof(udtToDto)] = udtToDto,
+            [nameof(ldtToDto)] = ldtToDto,
+            [nameof(dtToDto)] = dtToDto
+        };
 
-            var values = new Dictionary<string, object>()
-            {
-                [nameof(dto)] = dto,
-                [nameof(dtoPlus2)] = dtoPlus2,
-                [nameof(udt)] = udt,
-                [nameof(ldt)] = ldt,
-                [nameof(dt)] = dt,
-                [nameof(udtToDto)] = udtToDto,
-                [nameof(ldtToDto)] = ldtToDto,
-                [nameof(dtToDto)] = dtToDto
-            };
+        foreach (var v in values)
+        {
+            TestContext.Out.WriteLine(v.Key + " = " + v.Value.GetType().Name + " - " + (v.Value is System.DateTime vdt ? (object)vdt.Kind : v.Value is DateTimeOffset vdto ? vdto.Offset : null) + " : " + v.Value.ToString());
+        }
 
-            foreach (var v in values)
+        var table = from v1 in values
+                    from v2 in values
+                    orderby v1.Key != v2.Key
+                    select new { V1 = v1, V2 = v2 };
+
+        foreach (var row in table)
+        {
+            if (row.V1.Key == row.V2.Key)
             {
-                TestContext.Out.WriteLine(v.Key + " = " + v.Value.GetType().Name + " - " + (v.Value is System.DateTime vdt ? (object)vdt.Kind : v.Value is DateTimeOffset vdto ? vdto.Offset : null) + " : " + v.Value.ToString());
+                Assert.That(row.V1.Value, Is.SameAs(row.V2.Value));
             }
 
-            var table = from v1 in values
-                        from v2 in values
-                        orderby v1.Key != v2.Key
-                        select new { V1 = v1, V2 = v2 };
-
-            foreach (var row in table)
-            {
-                if (row.V1.Key == row.V2.Key)
-                {
-                    Assert.That(row.V1.Value, Is.SameAs(row.V2.Value));
-                }
-                
-                TestContext.Out.WriteLine($"{row.V1.Key} == {row.V2.Key} : {object.Equals(row.V1.Value, row.V2.Value)}");
-            }
+            TestContext.Out.WriteLine($"{row.V1.Key} == {row.V2.Key} : {object.Equals(row.V1.Value, row.V2.Value)}");
         }
     }
 }
