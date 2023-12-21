4e42b87bc71dd0228ebf6ea40fe87255f5172539
Valentino <valentino@asistar.it>
2023-12-21 09:11:41 +0100

    Refine date time test.

--- source/MValeTests.DateTime/DateTimeTest.cs
+++ source/MValeTests.DateTime/DateTimeTest.cs
@@ -5,6 +5,8 @@ using System.Linq;
 namespace MValeTests.DateTime;
 
 /// <summary>
+/// Microsoft reference:
+/// 
 ///     Constructs a DateTimeOffset from a DateTime. For Local and Unspecified kinds,
 ///     extracts the local offset. For UTC, creates a UTC instance with a zero offset.
 ///     public DateTimeOffset(DateTime dateTime) {
@@ -25,36 +27,69 @@ public class DateTimeTest
     [Test]
     public static void Test()
     {
-        var dto = DateTimeOffset.UtcNow;
-        var dtoPlus2 = dto.ToOffset(TimeSpan.FromHours(2));
-        var udt = dto.UtcDateTime;
-        var ldt = dto.LocalDateTime;
-        var dt = dto.DateTime;
-        var udtToDto = new DateTimeOffset(udt);
-        var ldtToDto = new DateTimeOffset(ldt);
-        var dtToDto = new DateTimeOffset(dt);
+        var ldto = DateTimeOffset.Now;
+        var udto = ldto.ToOffset(TimeZoneInfo.Utc.GetUtcOffset(ldto));
+        var udtoPlusRtz = udto.ToOffset(
+            TimeZoneInfo.GetSystemTimeZones()
+            .Where(i => i.GetUtcOffset(udto) != ldto.Offset)
+            .OrderBy(i => Random.Shared.NextDouble())
+            .First()
+            .GetUtcOffset(udto));
+
+        var udtoPlusLtz = udto.ToOffset(TimeZoneInfo.Local.GetUtcOffset(udto));
+        Assert.That(ldto, Is.EqualTo(udtoPlusLtz));
 
         var values = new Dictionary<string, object>()
         {
-            [nameof(dto)] = dto,
-            [nameof(dtoPlus2)] = dtoPlus2,
-            [nameof(udt)] = udt,
-            [nameof(ldt)] = ldt,
-            [nameof(dt)] = dt,
-            [nameof(udtToDto)] = udtToDto,
-            [nameof(ldtToDto)] = ldtToDto,
-            [nameof(dtToDto)] = dtToDto
+            [nameof(ldto)] = ldto,
+            [nameof(udto)] = udto,
+            [nameof(udtoPlusRtz)] = udtoPlusRtz
         };
+
+        foreach ((var key, var value) in values.ToList())
+        {
+            if (value is DateTimeOffset dateTimeOffset)
+            {
+                values.Add(key + "AsUdt", dateTimeOffset.UtcDateTime);
+                values.Add(key + "AsLdt", dateTimeOffset.LocalDateTime);
+                values.Add(key + "AsDt", dateTimeOffset.DateTime);
+            }
+        }
+
+        foreach ((var key, var value) in values.ToList())
+        {
+            if (value is System.DateTime dateTime)
+            {
+                values.Add(key + "ToDto", new DateTimeOffset(dateTime));
+            }
+        }
 
         foreach (var v in values)
         {
-            TestContext.Out.WriteLine(v.Key + " = " + v.Value.GetType().Name + " - " + (v.Value is System.DateTime vdt ? (object)vdt.Kind : v.Value is DateTimeOffset vdto ? vdto.Offset : null) + " : " + v.Value.ToString());
+            TestContext.Out.WriteLine(
+                v.Key
+                + " = "
+                + v.Value.GetType().Name
+                + " - "
+                + ((v.Value is System.DateTime vdt
+                    ? (object)vdt.Kind
+                    : v.Value is DateTimeOffset vdto
+                        ? vdto.Offset
+                        : null)
+                    ?.ToString() ?? string.Empty)
+                + " : "
+                + v.Value.ToString());
         }
 
-        var table = from v1 in values
-                    from v2 in values
-                    orderby v1.Key != v2.Key
-                    select new { V1 = v1, V2 = v2 };
+        var table = from row in (
+                from v1 in values
+                from v2 in values
+                select new { V1 = v1, V2 = v2 })
+            orderby row.V1.Key != row.V2.Key
+            group row
+            by string.Join("", new[] { row.V1.Key, row.V2.Key }.OrderBy(k => k))
+            into g
+            select g.First();
 
         foreach (var row in table)
         {
